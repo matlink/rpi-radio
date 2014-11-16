cfe45ad556b612c66a41994f69c91da011d9d487
Matlink <valois.mathieu@opmbx.org>
2014-11-16 17:14:47 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+mkdir -p /pirateradio/
+apt-get update -qq
+apt-get install -y -qq python python-pip git ffmpeg 
+pip install configparser
+cd /pirateradio
+git clone https://github.com/Make-Magazine/PirateRadio ./
+sed -i 's@/root/pifm@/pirateradio/pifm@g' ./PirateRadio.py
+sed -i "s@frequency.*@frequency = 88.2@g" ./pirateradio.conf
+sed -i "s@music_dir.*@music_dir = /media/music@g" ./pirateradio.conf
+cp ./pirate-radio /etc/init.d/pirate-radio
+chmod +x /etc/init.d/pirate-radio
+update-rc.d pirate-radio remove
