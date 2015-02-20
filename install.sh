c443d02f831f794b06ceb99ef60498ebdbb68bb6
matlink <matlink@matlink.fr>
2015-02-21 00:50:25 +0100

    [fix] commenting installation

--- install.sh
+++ install.sh
@@ -1,15 +1,23 @@
 #! /bin/bash
+echo "This will install pirate-radio in the /pirateradio folder."
+echo "Press any key to continue. CTRL+C to exit."
+read
 mkdir -p /pirateradio/
-apt-get update -qq
-apt-get install -y -qq python python-pip git ffmpeg 
+echo "Updating apt ..."
+apt-get update 
+echo "Installing dependancies ..."
+apt-get install -y python python-pip git ffmpeg 
 pip install configparser
+echo "Creating pirate-radio folder ..."
 cp ./pirate-radio /etc/init.d/pirate-radio
 cd /pirateradio
+echo "Downloading pirate-radio ..."
 git clone https://github.com/Make-Magazine/PirateRadio ./
+echo "Setting default configuration ..."
 sed -i 's@/root/pifm@/pirateradio/pifm@g' ./PirateRadio.py
 sed -i "s@frequency.*@frequency = 88.2@g" ./pirateradio.conf
 sed -i "s@music_dir.*@music_dir = /media/music@g" ./pirateradio.conf
-sed -i "s@shuffle.*@shuffle = False@g" ./pirateradio.conf
+sed -i "s@repeat_all.*@repeat_all = False@g" ./pirateradio.conf
 chmod +x /etc/init.d/pirate-radio
 update-rc.d pirate-radio remove
 mkdir -p /var/run/pirate-radio/
