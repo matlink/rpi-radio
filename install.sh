8f3d9686f9407f5fcdab208ad277d5eb9a4277bd
matlink <matlink@matlink.fr>
2015-02-21 00:45:56 +0100

    [fix] repeat is disabled by default

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ git clone https://github.com/Make-Magazine/PirateRadio ./
 sed -i 's@/root/pifm@/pirateradio/pifm@g' ./PirateRadio.py
 sed -i "s@frequency.*@frequency = 88.2@g" ./pirateradio.conf
 sed -i "s@music_dir.*@music_dir = /media/music@g" ./pirateradio.conf
+sed -i "s@shuffle.*@shuffle = False@g" ./pirateradio.conf
 chmod +x /etc/init.d/pirate-radio
 update-rc.d pirate-radio remove
 mkdir -p /var/run/pirate-radio/
