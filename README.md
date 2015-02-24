6a0315af418cf97ed9d4f193b356389179333d9c
matlink <matlink@matlink.fr>
2015-02-24 20:59:12 +0100

    [upd] Readme update and more quiet installer

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-rpi-radio
-=========
+Raspberry Pi Radio
+==================
+
+Un script d'installation pour faciliter le déploiement de la radio pour raspberry pi sur votre engin.
+
+Service
+-------
+sudo service pirate-radio ( start | stop | status )
+
+Configuration 
+-------------
+/pirateradio/pirateradio.conf
--- install.sh
+++ install.sh
@@ -2,17 +2,22 @@
 echo "This will install pirate-radio in the /pirateradio folder."
 echo "Press any key to continue. CTRL+C to exit."
 read
-mkdir -p /pirateradio/
+
 echo "Updating apt ..."
-apt-get update 
+apt-get -qq update 
+
 echo "Installing dependancies ..."
-apt-get install -y python python-pip git ffmpeg 
+apt-get install -y -qq python python-pip git ffmpeg 
 pip install configparser
+
 echo "Creating pirate-radio folder ..."
+mkdir -p /pirateradio/
 cp ./pirate-radio /etc/init.d/pirate-radio
 cd /pirateradio
+
 echo "Downloading pirate-radio ..."
 git clone https://github.com/Make-Magazine/PirateRadio ./
+
 echo "Setting default configuration ..."
 sed -i 's@/root/pifm@/pirateradio/pifm@g' ./PirateRadio.py
 sed -i "s@frequency.*@frequency = 88.2@g" ./pirateradio.conf
@@ -22,3 +27,5 @@ chmod +x /etc/init.d/pirate-radio
 update-rc.d pirate-radio remove
 mkdir -p /var/run/pirate-radio/
 chmod o+w /var/run/pirate-radio/
+
+echo "Done."
